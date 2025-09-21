f5683905b50f0f96df1387fd5397c34d672e6505
Gizachew Bayness <eleccrazy24@gmail.com>
2025-09-21 23:51:05 +0200

    Create paths.py for managing project directories and file paths

---
+++ src/research_assistant_chatbot/utils/paths.py
@@ -0,0 +1,16 @@
+"""
+File: paths.py
+Description: This module defines the file paths used in the project.
+Author: Gizachew Kassa
+Date: 21/09/2025
+"""
+
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ENV_FPATH = os.path.join(ROOT_DIR, ".env")
+
+
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+PUBLICATION_FPATH = os.path.join(DATA_DIR, "project_1_publications.json")
