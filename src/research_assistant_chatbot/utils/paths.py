c9ff240e5c0369e8c6836eb98e2254a57a2c9872
Gizachew Bayness <eleccrazy24@gmail.com>
2025-09-21 23:59:31 +0200

    Fix paths.py: correct ROOT_DIR resolution and reformat code

--- src/research_assistant_chatbot/utils/paths.py
+++ src/research_assistant_chatbot/utils/paths.py
@@ -7,10 +7,16 @@ Date: 21/09/2025
 
 import os
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    )
+)
 
 ENV_FPATH = os.path.join(ROOT_DIR, ".env")
 
 
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 PUBLICATION_FPATH = os.path.join(DATA_DIR, "project_1_publications.json")
+
+print(ROOT_DIR)
